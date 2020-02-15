2af79f4e200a18a7a421119ea575d490c0a19e45
Ehiremen Ekore <ehisekore@outlook.com>
2020-02-15 18:01:27 -0500

    Add files via upload

---
+++ foodHub/foodHub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  foodHub
+//
+//  Created by Ekore, Ehiremen Alex on 2/15/20.
+//  Copyright © 2020 Ekore, Ehiremen Alex. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
