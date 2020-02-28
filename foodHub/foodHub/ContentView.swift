3ccb40977051c17408a17bf16d1d1c1010b4c0d1
Ehis <ehisekore@outlook.com>
2020-02-27 20:35:04 -0500

    added firebase pods, testing firebase

--- foodHub/foodHub/ContentView.swift
+++ foodHub/foodHub/ContentView.swift
@@ -7,13 +7,76 @@
 //
 
 import SwiftUI
+import FirebaseFirestore
+
+let db = Firestore.firestore()
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Button(action: {
+                createFoodie()
+            }) {
+             Text("Create a foodie")
+                .font(.largeTitle)
+            }
+            .padding(10.0)
+            
+            Button(action: {
+                deleteCollection(collection: "foodies")
+            }) {
+             Text("Delete all foodies")
+                .font(.largeTitle)
+            }
+            .padding(10.0)
+            
+            Button(action: {
+                getCollection(collection: "foodies")
+            }) {
+             Text("Get all foodies")
+                .font(.largeTitle)
+            }
+            .padding(10.0)
+        }
     }
 }
 
+private func getCollection(collection: String){
+    db.collection(collection).getDocuments() { (querySnapshot, err) in
+        if let err = err {
+            print("Error getting documents: \(err)")
+        }
+        else {
+            for document in querySnapshot!.documents {
+                print("\(document.documentID) => \(document.data())")
+            }
+        }
+    }
+}
+    
+private func deleteCollection(collection: String){
+    db.collection(collection).getDocuments() { (querySnapshot, err) in
+        if let err = err {
+            print("Error getting documents: \(err)")
+        } else {
+            for document in querySnapshot!.documents {
+            print("Deleting \(document.documentID) => \(document.data())")
+                document.reference.delete()
+            }
+        }
+    
+    }
+}
+
+private func createFoodie(){
+    let foodieRef = db.collection("foodies")
+    
+    foodieRef.document().setData([
+        "name": "McDonalds",
+        "city": "West Haven",
+        "zipCode": 06516
+    ])
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
