437d0574ba064115615dd26b94017249c0d4fb67
Ehis <ehisekore@outlook.com>
2020-03-06 20:10:53 -0500

    trying to make creating users dynamic

---
+++ foodHub/foodHub/EaterScreens/CreateEaterView.swift
@@ -0,0 +1,21 @@
+//
+//  CreateEaterView.swift
+//  foodHub
+//
+//  Created by Ekore, Ehiremen Alex on 3/6/20.
+//  Copyright © 2020 Ekore, Ehiremen Alex. All rights reserved.
+//
+
+import SwiftUI
+
+struct CreateEaterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreateEaterView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateEaterView()
+    }
+}
---
+++ foodHub/foodHub/Model/EaterUser.swift
@@ -0,0 +1,32 @@
+//
+//  EaterUser.swift
+//  foodHub
+//
+//  Created by Ekore, Ehiremen Alex on 3/6/20.
+//  Copyright © 2020 Ekore, Ehiremen Alex. All rights reserved.
+
+class EaterUser{
+    var name: String
+    var address: String
+    var email: String
+    var isActive: Bool
+    var isAdmin: Bool
+    var zipCode: String
+    
+    init? (_ name: String, _ address: String, _ email: String, isAdmin: Bool, zipCode: String){
+        if name.isEmpty || email.isEmpty {
+            return nil
+        }
+        if isAdmin != true {
+            self.isAdmin = false
+        }
+        else {
+            self.isAdmin = true
+        }
+        self.name = name
+        self.address = address
+        self.email = email
+        self.isActive = true
+        self.zipCode = zipCode
+    }
+}
