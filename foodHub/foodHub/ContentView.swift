81f887b7b0d5be1b65ede029f31ea71fd0ab90b0
Ehiremen Ekore <eekor1@unh.newhaven.edu>
2020-07-28 12:06:55 -0400

    testing git from a clone on a different machine

--- foodHub/foodHub/ContentView.swift
+++ foodHub/foodHub/ContentView.swift
@@ -5,7 +5,7 @@
 //  Created by Ekore, Ehiremen Alex on 2/15/20.
 //  Copyright © 2020 Ekore, Ehiremen Alex. All rights reserved.
 //
-
+// back on 07/28 :)
 import SwiftUI
 import FirebaseFirestore
 
