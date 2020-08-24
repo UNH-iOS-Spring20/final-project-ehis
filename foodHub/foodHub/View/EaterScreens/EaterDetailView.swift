0ce3641ea48c4f5d23fb153eb650ae5a037b89a1
Ehiremen Ekore <eekor1@unh.newhaven.edu>
2020-08-24 00:20:15 -0400

    implemented nav links for EaterUser favorite stores

--- foodHub/foodHub/View/EaterScreens/EaterDetailView.swift
+++ foodHub/foodHub/View/EaterScreens/EaterDetailView.swift
@@ -12,59 +12,65 @@ import FirebaseFirestore
 struct EaterDetailView: View {
     @EnvironmentObject var sessionUser: SessionUser
     @ObservedObject var eater: EaterUser
+    @ObservedObject private var foodies = FirebaseCollection<FoodieUser> (collectionRef: foodiesCollectionRef)
     
     init(eater: EaterUser) {
         self.eater = eater
     }
     
     var body: some View {
-        VStack (alignment: .leading, spacing: 10) {
-            HStack{
-                ImageViewController(imageUrl: eater.photo)
+        NavigationView{
+            VStack (alignment: .leading, spacing: 10) {
+                HStack{
+                    ImageViewController(imageUrl: eater.photo)
+                    
+                    Text(eater.name)
+                        .font(.title)
+                }
+                HStack {
+                    Text("Active: ")
+                    Image(systemName: (eater.isActive ? "checkmark.square" : "square"))
+                }
+                HStack {
+                    Text("Admin: ")
+                    Image(systemName: (eater.isAdmin ? "checkmark.square" : "square"))
+                }
                 
-                Text(eater.name)
-                    .font(.title)
-            }
-            HStack {
-                Text("Active: ")
-                Image(systemName: (eater.isActive ? "checkmark.square" : "square"))
-            }
-            HStack {
-                Text("Admin: ")
-                Image(systemName: (eater.isAdmin ? "checkmark.square" : "square"))
-            }
-            
-            HStack{
-                Text("Email: ")
-                Text(eater.email)
-            }
-            
-            HStack{
-                Text("Zipcode: ")
-                Text(eater.zipCode)
-            }
-            Text("\n\(eater.name)'s Favorite Foodies")
-                .bold()
-            List {
-                ForEach(Array(eater.favorites), id: \.self) { index in
-                    Text(index)
-                    //                        NavigationLink(
-                    //                        destination: FoodieDetailView(foodie: foodiesCollectionRef.document(index) as! FoodieUser)){
-                    //                        Text(index)
-                    //                        }
+                HStack{
+                    Text("Email: ")
+                    Text(eater.email)
                 }
-            }
-            if sessionUser.validateEater(eater: eater) {
-                NavigationLink(
-                    destination: EditEaterView(eater: self.eater)
-                ){
-                    Text("Edit info")
+                
+                HStack{
+                    Text("Zipcode: ")
+                    Text(eater.zipCode)
                 }
-            }
-            
-        }.padding()
-            .font(.body).padding()
-            .navigationBarTitle("About \(eater.name)")
+                Text("\n\(eater.name)'s Favorite Foodies")
+                    .bold()
+                List {
+                    ForEach(Array(eater.favorites), id: \.self) { index in
+                        NavigationLink(
+                            destination: FoodieDetailView(foodie: self.foodies.items[
+                                self.foodies.items.firstIndex(where: { $0.id == index })!
+                                ])){
+                                    Text(self.foodies.items[
+                                        self.foodies.items.firstIndex(where: { $0.id == index })!
+                                    ].name)
+                        }
+                    }
+                }
+                if sessionUser.validateEater(eater: eater) {
+                    NavigationLink(
+                        destination: EditEaterView(eater: self.eater)
+                    ){
+                        Text("Edit info")
+                    }
+                }
+                
+            }.padding()
+                .font(.body).padding()
+                .navigationBarTitle("About \(eater.name)")
+        }
     }
 }
 
